19a7de40e74a9d1952c03a53fa119df003dab8f3
Diego Borges <diegorp2013@hotmail.com>
2023-05-21 23:24:39 -0300

    adjust server

--- src/routes/memories.ts
+++ src/routes/memories.ts
@@ -23,6 +23,7 @@ export async function memoriesRoutes(app: FastifyInstance) {
         coverUrl: memory.coverUrl,
         excerpt: memory.content.substring(0, 115).concat('...'),
         createdAt: memory.createdAt,
+        isPublic: memory.isPublic,
       }
     })
   })
@@ -69,42 +70,48 @@ export async function memoriesRoutes(app: FastifyInstance) {
   })
 
   app.put('/memories/:id', async (request, reply) => {
-    const paramsSchema = z.object({
-      id: z.string().uuid(),
-    })
+    try {
+      const paramsSchema = z.object({
+        id: z.string(),
+      })
 
-    const { id } = paramsSchema.parse(request.params)
+      const { id } = paramsSchema.parse(request.params)
 
-    const bodySchema = z.object({
-      content: z.string(),
-      coverUrl: z.string(),
-      isPublic: z.coerce.boolean().default(false),
-    })
+      const bodySchema = z.object({
+        content: z.string(),
+        coverUrl: z.string(),
+        isPublic: z.coerce.boolean().default(false),
+      })
 
-    const { content, coverUrl, isPublic } = bodySchema.parse(request.body)
+      const { content, coverUrl, isPublic } = bodySchema.parse(request.body)
 
-    let memory = await prisma.memory.findUniqueOrThrow({
-      where: {
-        id,
-      },
-    })
+      let memory = await prisma.memory.findUniqueOrThrow({
+        where: {
+          id,
+        },
+      })
 
-    if (memory.userId !== request.user.sub) {
-      return reply.status(401).send()
+      console.log('memory', memory)
+
+      if (memory.userId !== request.user.sub) {
+        return reply.status(401).send()
+      }
+
+      memory = await prisma.memory.update({
+        where: {
+          id,
+        },
+        data: {
+          content,
+          coverUrl,
+          isPublic,
+        },
+      })
+
+      return memory
+    } catch (error) {
+      console.error('errou aqui', error)
     }
-
-    memory = await prisma.memory.update({
-      where: {
-        id,
-      },
-      data: {
-        content,
-        coverUrl,
-        isPublic,
-      },
-    })
-
-    return memory
   })
 
   app.delete('/memories/:id', async (request, reply) => {
--- src/routes/upload.ts
+++ src/routes/upload.ts
@@ -9,37 +9,41 @@ const pump = promisify(pipeline)
 
 export async function uploadRoutes(app: FastifyInstance) {
   app.post('/upload', async (request, reply) => {
-    const upload = await request.file({
-      limits: {
-        fileSize: 5_242_880, // 5mb
-      },
-    })
+    try {
+      const upload = await request.file({
+        limits: {
+          fileSize: 5_242_880, // 5mb
+        },
+      })
 
-    if (!upload) {
-      return reply.status(400).send()
+      if (!upload) {
+        return reply.status(400).send()
+      }
+
+      const mimeTypeRegex = /^(image|video)\/[a-zA-Z]+/
+      const isValidFileFormat = mimeTypeRegex.test(upload.mimetype)
+
+      if (!isValidFileFormat) {
+        return reply.status(400).send()
+      }
+
+      const fileId = randomUUID()
+      const extension = extname(upload.filename)
+
+      const fileName = fileId.concat(extension)
+
+      const writeStream = createWriteStream(
+        resolve(__dirname, '..', '..', 'uploads', fileName),
+      )
+
+      await pump(upload.file, writeStream)
+
+      const fullUrl = request.protocol.concat('://').concat(request.hostname)
+      const fileUrl = new URL(`/uploads/${fileName}`, fullUrl).toString()
+
+      return { fileUrl }
+    } catch (error) {
+      console.log('Error upload', error)
     }
-
-    const mimeTypeRegex = /^(image|video)\/[a-zA-Z]+/
-    const isValidFileFormat = mimeTypeRegex.test(upload.mimetype)
-
-    if (!isValidFileFormat) {
-      return reply.status(400).send()
-    }
-
-    const fileId = randomUUID()
-    const extension = extname(upload.filename)
-
-    const fileName = fileId.concat(extension)
-
-    const writeStream = createWriteStream(
-      resolve(__dirname, '..', '..', 'uploads', fileName),
-    )
-
-    await pump(upload.file, writeStream)
-
-    const fullUrl = request.protocol.concat('://').concat(request.hostname)
-    const fileUrl = new URL(`/uploads/${fileName}`, fullUrl).toString()
-
-    return { fileUrl }
   })
 }
